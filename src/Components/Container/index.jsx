2d848302af6b440d3a9e07d84496f13cc6fe90ff
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2020-11-23 12:51:29 -0300

    🎉 initial commit

---
+++ src/Components/Container/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import Titulo from "../Titulo";
+import Conta from "../Conta";
+
+import styled from 'styled-components';
+import { fundoClaro } from '../../UI/variaveis'
+
+const Container = styled.div`
+  background-color: ${fundoClaro};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export default () => {
+  return (
+    <Container>
+      <Titulo>Olá Fulano!</Titulo>
+      <Conteudo>
+        <Conta />
+      </Conteudo>
+    </Container>
+  );
+};
---
+++ src/UI/variaveis.js
@@ -0,0 +1,3 @@
+export const corPrimaria = '#41d4be';
+export const fundoClaro = '#f1f1f1';
+export const textoFundoClaro = 'grey';
